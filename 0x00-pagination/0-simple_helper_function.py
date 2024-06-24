8ab48960b38e076da3dd6e19e670af24623ac063
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-06-24 09:10:50 -0700

    done-task-0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
-"""
-    Return a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes to return in a list for those
-    particular pagination parameters."""
 def index_range(page, page_size):
-    """
-    Return a tuple of size two containing a start index and an end index
-    corresponding to the range of indexes to return in a list for those
-    particular pagination parameters."""
-    result = index_range(page, page_size)
-    return (result)
-
+    fin = (page - 1) * page_size
+    lin = fin + page_size
+    return (fin, lin)
