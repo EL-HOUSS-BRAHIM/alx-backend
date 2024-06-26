eb9ee0fd18280a13bcd30e4121b8e3a3422323cf
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-06-26 16:31:32 -0700

    task3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+class LRUCache(BaseCaching):
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+    def put(self, key, item):
+        if key is None or item in None:
+            return
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        self.cache_data[key] = item
+        if (len(self.cache_data) > self.MAX_ITEMS:
+            oldest_key, oldest_value = self.cache_data.popitem(last=False)
+            print(f"DISCARD: {oldest_key}")
+    def get(self, key):
+        """
+        Return the value in self.cache_data linked to key.
+        """
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)  # Mark as recently used
+            return self.cache_data[key]
+        return None
