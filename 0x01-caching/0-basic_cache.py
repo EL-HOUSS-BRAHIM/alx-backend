4f7ee115e8fc888febdefb341adbd5fd68aa9ddc
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-06-26 05:07:48 -0700

    task-0_done

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+""" """
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return (None)
+        return self.cache_data[key]
