b2b9bb9b6ad4ab629c8be9456db891202cc78ff1
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-07-02 15:23:53 -0700

    task3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template, request
-from flask_babel import Babel
+from flask_babel import Babel, gettext as _
 
 class Config:
     LANGUAGES = ['en', 'fr']
@@ -18,7 +18,7 @@ def get_locale():
 
 @app.route('/')
 def index():
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
