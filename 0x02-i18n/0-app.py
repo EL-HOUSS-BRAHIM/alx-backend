d21fd2fe7276ab79c4f6088e7e16422fea139cc1
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-07-02 14:00:26 -0700

    task1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 from flask import Flask, render_template
+from flask_babel import Babel
+
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 app = Flask(__name__)
+app.config.from_object('Config')
+babel = Babel(app)
 
 @app.route('/')
 def index():
