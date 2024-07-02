9d90380f449c51ba95de08961d42cb43e8642d46
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-07-02 15:57:00 -0700

    task4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -14,7 +14,7 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    locale = request.args.get('local')
+    locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
         return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
