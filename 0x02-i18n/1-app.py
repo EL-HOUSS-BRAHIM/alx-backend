175d586362c4b22d83cc17dda6d53235f67c4c81
EL-HOUSS-BRAHIM <bross.or.of@gmail.com>
2024-07-02 22:15:51 +0100

    task1

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -8,12 +8,13 @@ class Config:
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 app = Flask(__name__)
-app.config.from_object('Config')
+app.config.from_object(Config)
+
 babel = Babel(app)
 
 @app.route('/')
 def index():
-    return render_template('0-index.html')
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    return render_template('1-index.html')
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
